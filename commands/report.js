08b77024005e0fb4cf5288acb5bb296101e78873
Cart <cartrigger@gmail.com>
2023-02-15 21:33:41 -0500

    changes

---
+++ commands/report.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, ModalBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('report')
+        .setDescription('Lets you report players.'),
+    async execute(interaction) {
+        const modal = new ModalBuilder()
+            .setCustomId('myModal')
+            .setTitle('My Modal');
+        const favoriteColorInput = new TextInputBuilder()
+            .setCustomId('favoriteColorInput')
+            .setLabel("What's your favorite color?")
+            .setStyle(TextInputStyle.Short);
+        const hobbiesInput = new TextInputBuilder()
+            .setCustomId('hobbiesInput')
+            .setLabel("What's some of your favorite hobbies?")
+            .setStyle(TextInputStyle.Paragraph);
+        const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
+        const secondActionRow = new ActionRowBuilder().addComponents(hobbiesInput);
+        modal.addComponents(firstActionRow, secondActionRow);
+        await interaction.showModal(modal);
+    },
+};
